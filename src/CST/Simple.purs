56050c27b60552a87303a8aacb73437ef48de469
Jan Vincent Liwanag <jvliwanag@gmail.com>
2020-11-11 20:40:01 +0800

    Add project reexport

---
+++ src/CST/Simple.purs
@@ -0,0 +1,230 @@
+module CST.Simple
+       ( module E
+       ) where
+
+import CST.Simple.Internal.Binder
+  ( Binder
+  , bndrWildcard
+  , bndrVar
+  , bndrNamed
+  , bndrConstructor
+  , bndrBoolean
+  , bndrChar
+  , bndrString
+  , bndrNumber
+  , bndrArray
+  , bndrRecord
+  , bndrTyped
+  , bndrOp
+  ) as E
+
+import CST.Simple.Internal.CodegenError
+  ( CodegenError(..)
+  ) as E
+
+import CST.Simple.Internal.CommonOp
+  ( class RightSingleArrow
+  , (*->)
+  , class LeftSingleArrow
+  , (*<-)
+  , class RightDoubleArrow
+  , (*=>)
+  , class DoubleColon
+  , (*::)
+  , class Equals
+  , (*=)
+  , class Space
+  , (*-)
+  ) as E
+
+import CST.Simple.Internal.CommonWords
+  ( LetBindingsB
+  , _let
+  , _do
+  , _letd
+  , AdoStatements
+  , _ado
+  , class InOp
+  , _in
+  , _where
+  ) as E
+
+import CST.Simple.Internal.Declaration
+  ( Declaration
+  , declData
+  , declType
+  , declNewtype
+  , declClass
+  , declInstance
+  , declInstanceChain
+  , declDerive
+  , declDeriveNewtype
+  , declSignature
+  , declValue
+  , declInfix
+  , declForeignValue
+  , declForeignData
+  , declForeignKind
+  , DataCtor
+  , dataCtor
+  , Instance
+  , instance_
+  , InstanceBinding
+  , instanceBSig
+  , instanceBName
+  , FixityOp
+  , fixityOpValue
+  , fixityOpType
+  , Fixity(..)
+  ) as E
+
+
+import CST.Simple.Internal.Expression
+  ( Expr
+  , exprIdent
+  , exprIdentN
+  , exprIdent1
+  , exprIdent2
+  , exprIdent3
+  , exprIdent4
+  , exprIdent5
+  , exprIdent6
+  , exprCons
+  , exprConsN
+  , exprCons1
+  , exprCons2
+  , exprCons3
+  , exprCons4
+  , exprCons5
+  , exprCons6
+  , exprBoolean
+  , exprChar
+  , exprString
+  , exprInt
+  , exprNumber
+  , exprArray
+  , exprRecord
+  , exprTyped
+  , exprOp
+  , exprOpName
+  , exprNegate
+  , exprRecordAccess
+  , exprRecordAccessN
+  , exprRecordUpdate
+  , exprApp
+  , exprLambda
+  , exprIfThenElse
+  , exprCaseOfN
+  , exprCaseOf1
+  , exprCaseOf2
+  , exprCaseOf3
+  , exprCaseOf4
+  , exprCaseOf5
+  , exprCaseOf6
+  , exprLetIn
+  , exprDo
+  , exprAdo
+  , RecordUpdate
+  , recordUpdate
+  , recordUpdateBranch
+  , CaseOfBranch
+  , caseOfBranchN
+  , caseOfBranch1
+  , caseOfBranch2
+  , caseOfBranch3
+  , caseOfBranch4
+  , caseOfBranch5
+  , caseOfBranch6
+  , LetBinding
+  , letSig
+  , letName
+  , letPattern
+  , Where
+  , whr_
+  , whr
+  , Guarded
+  , grd_
+  , grdUncond
+  , DoStatement
+  , doLet
+  , doDiscard
+  , doBind
+  , valueBindingFields
+  ) as E
+
+import CST.Simple.Internal.Import
+  ( class AsImport
+  ) as E
+
+import CST.Simple.Internal.Kind
+  ( Kind
+  , knd
+  , kndArrow
+  , kndRow
+  ) as E
+
+import CST.Simple.Internal.ModuleBuilder
+  ( ModuleBuilderT
+  , ModuleBuilder
+  , liftModuleBuilder
+  , addImport
+  , addForeignBinding
+  , mkName
+  , mkQualName
+  , mkQualConstructorName
+  ) as E
+
+import CST.Simple.Internal.NamedBinders
+  ( NamedBinders(..)
+  , namedBinders1
+  , nbAddBinder
+  ) as E
+
+import CST.Simple.Internal.RecordLabeled
+  ( RecordLabeled
+  , runRecordLabeled
+  , recField
+  , recPun
+  ) as E
+
+import CST.Simple.Internal.Type
+  ( Type
+  , typ
+  , typVar
+  , typCons
+  , typString
+  , typRow
+  , typRow_
+  , typRecord
+  , typRecord_
+  , typApp
+  , typForall
+  , typArrow
+  , typKinded
+  , typOp
+  , typConstrained
+  , Constraint
+  , cnst
+  ) as E
+
+import CST.Simple.Internal.TypeVarBinding
+  ( TypeVarBinding
+  , runTypeVarBinding
+  , tvb
+  , tvbKinded
+  ) as E
+
+import CST.Simple.Names
+  ( TypeName
+  , ConstructorName
+  , ClassName
+  , KindName
+  , TypedConstructorName(..)
+  , TypeOpName
+  , ValueOpName
+  , class ReadName
+  , Ident
+  , Label(..)
+  , ModuleName
+  , QualifiedName(..)
+  ) as E
